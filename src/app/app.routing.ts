104ebafaa5b5abbde033d234bc01e5b38829d7b5
Rafael Correia <rafaelcbm@gmail.com>
2018-10-01 09:15:34 -0300

    - Inclusão de arquivos/config desfeitos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 	imports: [
 		RouterModule.forRoot([{
 			path: '',
-			redirectTo: 'chat-full',
+			redirectTo: 'home',
 			pathMatch: 'full'
 		},
 		{
